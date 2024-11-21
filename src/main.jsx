6676db7717db91252b48fbbc3c60b0656b62a7fb
Himel Islam <himel7100@gmail.com>
2024-11-21 21:52:22 +0600

    mod: removed unnessary codes

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Dashboard from '../src/app/dashboard/dashboard'
-import Home from './components/pages/home'
-import Referrer from './components/pages/referrer'
-import Login from './components/pages/login'
-import Signup from './components/pages/signup'
-
+import { RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import RoleSelection from './components/pages/roleSelection'
-import * as path from 'path'
-import BusinessSetup from './components/pages/businessSetup'
-import ReferrerSetup from './components/pages/referrerSetup'
+import { appRoutes } from './Routes'
 
 const queryClient = new QueryClient()
 
-import { appRoutes } from './Routes'
 
 createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
