6cb088f79534fae840c33f84688bd30013adc85d
Himel Islam <himel7100@gmail.com>
2025-05-12 03:25:51 +0600

    mod: zapier test event added

---
+++ src/api/integrationApi.js
@@ -0,0 +1,39 @@
+import { useUser } from "@/context/usercontext";
+import axios from "axios";
+
+const API_URL = `${import.meta.env.VITE_API_URL}/api/integration`;
+const user = JSON.parse(localStorage.getItem('user'))
+
+
+//Save Zapier Webhook URL
+const saveZapierURL = async (data) => {
+    const user = JSON.parse(localStorage.getItem('user'))
+
+    const response = await axios.post(API_URL + "/saveZapierURL", data, {
+        headers: {
+            Authorization: `Bearer ${user?.token}`
+        }
+    });
+
+    return response.data;
+};
+
+//Test Zapier Webhook URL
+const testZapierURL = async (data) => {
+    const user = JSON.parse(localStorage.getItem('user'))
+
+    const response = await axios.post(API_URL + "/testZapierURL", data, {
+        headers: {
+            Authorization: `Bearer ${user?.token}`
+        }
+    });
+
+    return response.data;
+};
+
+const integrationApi = {
+    saveZapierURL,
+    testZapierURL
+};
+
+export default integrationApi;
